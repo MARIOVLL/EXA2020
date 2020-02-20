a684bb95a0b794d21a392c6337748d3aa9969f89
MARIOVLL <52045187+MARIOVLL@users.noreply.github.com>
2020-02-20 10:56:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-alert('hola');
-var etiqueta;
-function onloadFcn(){
-	etiqueta=document.getElementById("led");
-	etiqueta.innerHTML="led 1";
-}
+//alert('hola');
+//var etiqueta;
+//function onloadFcn(){
+//	etiqueta=document.getElementById("led");
+//	etiqueta.innerHTML="led 1";
+//}
 
 
  // Create a client instance
@@ -28,78 +28,75 @@ function onloadFcn(){
   topic_rx="test";
   topic_tx="led"
   // called when the client connects
-  function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
-    console.log("onConnect");
-	
-    client.subscribe(topic_rx);
-    message = new Paho.MQTT.Message("don teno");
-    message.destinationName = topic_tx;
-    
-	
-  }
 
-  function doFail(e){
-    console.log(e);
+// called when the client connects
+function onConnect() {
+// Once a connection has been made, make a subscription and send a message.
+console.log("onConnect");
 	
-  }
+client.subscribe(topic_rx); // raspebrry
+message = new Paho.MQTT.Message("ll:Hello: CloudMQTT");
+message.destinationName = topic_tx; // recibe
+}
 
-  // called when the client loses its connection
-  function onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-      console.log("onConnectionLost:"+responseObject.errorMessage);
-    }
-  }
+function doFail(e){
+console.log(e);
+}
 
-  // called when a message arrives
-  function onMessageArrived(message) {
-    console.log("nuevo mensaje:"+message.payloadString);
-	
-		accion(message.payloadString);
-  }
-  
-    // called when a message arrives
-  function sendMessage(msg) {
-    message = new Paho.MQTT.Message(msg);
-    message.destinationName = topic_tx;
-    client.send(message);
-	
-  }
+// called when the client loses its connection
+var etiqueta;
+function onloadFcn(){
+etiqueta=document.getElementById("led");
+etiqueta.innerHTML = "LUZ";
 
-    // called when a message arrives
-  function led_on() {
-	sendMessage('led=1')
-	//var prueba = "LED PRENDIDO";
-	//document.write(prueba);	
-	var Resultado1;
-	Resultado1="PRENDIDO";
-	document.getElementById('res').value=Resultado1;
-	console.log(res)
-  }
-  function led_off() {
-	sendMessage('led=0')
-	//var prueba = "LED APAGADO";
-	//document.write(prueba);	
-	var Resultado1;
-	Resultado1="APAGADO";
-	document.getElementById('res').value=Resultado1;
-	console.log(res)
-  }
-  
-  
- 
-  
-  function accion(msg){
+}
+function onConnectionLost(responseObject) {
+if (responseObject.errorCode !== 0) {
+console.log("onConnectionLost:"+responseObject.errorMessage);
+}
+}
+// called when a message arrives
+function onMessageArrived(message) {
+console.log("Mensaje entregado:"+message.payloadString);
+	estado= message.payloadString;
+	if(estado == 'apagado'){
+	document.getElementById("led_state").innerHTML ='Sensor Apagado';
+	}
+	else if (estado=='encendido'){
+	document.getElementById("led_state").innerHTML ='Sensor Encendido';
+	}
+action(message.payloadString);
+//sensor=document.getElementById("sensor_val");
+//sensor.innerHTML=message.payloadString;
+}
+// called when a message arrives
+function sendMessage(msg) {
+message = new Paho.MQTT.Message(msg);
+message.destinationName = topic_tx;
+client.send(message);
+
+}
+// called when a message arrives
+function led() {
+sendMessage('led')
+console.log("Mensaje entregado:"+message.payloadString);
+}
+function led_on(){
+sendMessage('led=1')
+}
+function led_off(){
+sendMessage('led=0')
+}
+function action(msg) {
 mensaje=msg.split('=');
-    if(mensaje[0]=='i')
-        document.getElementById('sensor_i').innerHTML=mensaje[1];
-    if(mensaje[0]=='p')
-        document.getElementById('sensor_p').innerHTML=mensaje[1];
-    if(mensaje[0]=='l')
-        document.getElementById('sensor_l').innerHTML=mensaje[1];
-      
-		
-  }
+if(mensaje[0]=='i')
+	document.getElementById('sensor_i').innerHTML=mensaje[1];
+if(mensaje[0]=='p')
+	document.getElementById('sensor_p').innerHTML=mensaje[1];
+if(mensaje[0]=='l')
+	document.getElementById('sensor_l').innerHTML=mensaje[1];
+}
+
 y=0;
 	function led1() {
 		if (y == 0){
@@ -114,5 +111,3 @@ y=0;
 	
 	console.log("Te enviaron:"+message.payloadString);	
   }
-
-
